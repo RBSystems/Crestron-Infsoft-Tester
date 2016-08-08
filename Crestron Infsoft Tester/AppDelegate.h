e9ed9e434bc78d722550875d26e019142eb6219f
Victor <v4wang@ucsd.edu>
2016-08-08 14:58:14 -0400

    Initial Commit

---
+++ Crestron Infsoft Tester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Crestron Infsoft Tester
+//
+//  Created by Victor Wang on 8/8/16.
+//  Copyright © 2016 Victor Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Crestron Infsoft Tester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crestron Infsoft Tester
+//
+//  Created by Victor Wang on 8/8/16.
+//  Copyright © 2016 Victor Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
