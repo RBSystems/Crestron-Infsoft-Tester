37ac66e8083e9d6658ee0a31c488b25b99e0aa10
Victor <vwang1111@berkeley.edu>
2016-08-09 15:48:04 -0400

    Added permission to use location data.

--- Crestron Infsoft Tester/ViewController.m
+++ Crestron Infsoft Tester/ViewController.m
@@ -28,8 +28,14 @@
     _mapView = [[MapView alloc] initWithFrame:CGRectMake(0, 150, self.view.frame.size.width, self.view.frame.size.height)];
     _mapView.clipsToBounds = TRUE;
     _mapView.apiKey = @"d1eebea2-380f-42ad-aa3f-00844f5d37fc";
-    _mapView.displayPOIs = TRUE;
-    _mapView.DisplayButtonMyPosition = TRUE;
+    [_mapView setMinZoomLevel:25];
+    [_mapView setMaxZoomLevel:15];
+    [_mapView setDisplayButton3D:TRUE];
+    [_mapView setDisplayButtonLevelWheel:TRUE];
+    [_mapView setDisplayPOIs:TRUE];
+    [_mapView setDisplayButtonMyPosition:TRUE];
+    
+    
     
     [self.view insertSubview:_mapView atIndex:0];
 }
@@ -43,6 +49,7 @@
     if(location == NULL || !location.valid)
         return;
     
+    //update the labels in the main view with current position
     NSString *latString = [NSString stringWithFormat:@"%lf", location.latitude];
     _lattitudeLabel.text = latString;
     NSString *longString = [NSString stringWithFormat:@"%lf", location.longitude];
@@ -50,6 +57,7 @@
     
     NSLog(@"my location is lat=%lf, lon=%lf, level=%d", location.latitude, location.longitude, location.level);
     
+    //move the map to the correct location
     MyLocation* myLocation = [MyLocation new];
     myLocation.latitude = location.latitude;
     myLocation.longitude = location.longitude;
@@ -61,6 +69,17 @@
     [_mapView animateToMyLocation:myLocation];
 }
 
+//refresh the location labels to the crrent position
+- (IBAction)refreshLocation:(id)sender{
+    ILLocation *currentLoc = _locationManager.getLastKnownLocation;
+    
+    NSString *latString = [NSString stringWithFormat:@"%lf", currentLoc.latitude];
+    _lattitudeLabel.text = latString;
+    NSString *longString = [NSString stringWithFormat:@"%lf", currentLoc.longitude];
+    _longitudeLabel.text = longString;
+}
+
+//record current position into memory
 - (IBAction)recordNewLocation:(id)sender{
     ILLocation *currentLoc = _locationManager.getLastKnownLocation;
     
@@ -75,6 +94,7 @@
     _numPositionLabel.text = [NSString stringWithFormat:@"%d", [_numPositionLabel.text intValue]+1];
 }
 
+//remove last location data recorded
 - (IBAction)deleteLastLocation:(id)sender{
     if(resultArr.count >= 2){
         [resultArr removeLastObject];
@@ -83,11 +103,13 @@
     }
 }
 
+//send location data through email
 - (IBAction)emailLocationData:(id)sender{
     if([MFMailComposeViewController canSendMail]) {
         MFMailComposeViewController *mailCont = [[MFMailComposeViewController alloc] init];
         mailCont.mailComposeDelegate = self;
         
+        //build location data in csv format
         NSMutableString *resultStr = [[NSMutableString alloc]init];
         for(NSString *str in resultArr){
             [resultStr appendString:str];
@@ -102,7 +124,12 @@
     }
 }
 
+//after the email interface has been dismissed
 - (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error {
+    //clear out location data
+    [resultArr removeAllObjects];
+    _numPositionLabel.text = [NSString stringWithFormat:@"%d", 0];
+    
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
